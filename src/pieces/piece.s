0931106e3f3595e84f3283fcf0d8f63fd81992f6
Mathias Helsen <helsen.mathias@gmail.com>
2021-10-19 19:19:02 +0200

    removing old files

--- src/pieces/piece.s
+++
@@ -1,7 +0,0 @@
-!zone piece
-
-;; Look if a piece of opposite color is in the specified square
-isOppositePieceThere:
-
-;; Look if a peice of the players color is in the specified square
-isPlayerPieceThere:
