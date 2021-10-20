889a3c8a957dfcea7092721e9aef9795a81deb50
Mathias Helsen <helsen.mathias@gmail.com>
2021-10-20 19:34:28 +0200

    added moves to the game

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 
 #include "board.h"
 #include "piece.h"
+#include "move.h"
 
 int main(void)
 {
@@ -11,6 +12,9 @@ int main(void)
   printf("Allocated board at: %p\n", myBoard);
 
   deleteBoard(myBoard);
+
+  t_move myMove_A = {.x = 2, .y = -1, .nextMove = NULL};
+  t_move myMove_B = {.x = 0, .y = 2,  .nextMove = &myMove_A};
 
   return EXIT_SUCCESS;
 }
